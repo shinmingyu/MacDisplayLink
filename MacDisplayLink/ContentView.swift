a8237657139c634dbbdbae182c06068d9beecea9
shinmingyu <shin6752@gmail.com>
2025-12-01 13:39:02 +0900

    Initial Commit

---
+++ MacDisplayLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacDisplayLink
+//
+//  Created by 신민규 on 12/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacDisplayLink/MacDisplayLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacDisplayLinkApp.swift
+//  MacDisplayLink
+//
+//  Created by 신민규 on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MacDisplayLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
